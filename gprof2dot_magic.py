4dadb0393d41a9901c09c4d07f1f3574e4251d7e
Mattijn van Hoek <mattijn@gmail.com>
2019-07-18 22:22:14 +0800

    include requirements

--- gprof2dot_magic.py
+++ gprof2dot_magic.py
@@ -5,7 +5,7 @@ magic function that profile any statement as a dot graph using cProfile, gprof2d
 should give you a nice dot profile rendered as svg
 """
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 
 import cProfile
@@ -23,17 +23,17 @@ def gprof2dot(line):
         tf_directory = os.path.dirname(tf.name)
         tf_basename = os.path.basename(tf.name)
         dot_fp = os.path.join(tf_directory, '{}.dot'.format(tf_name_no_ext))
-        
+
         cProfile.run(statement=line, filename=tf.name)
         sys.argv = ['gprof2dot.py', '-f', 'pstats', '-o', dot_fp, tf.name]
         gprof2dot_source.main()
-    
+
     # read dot file and parse as graph object
     with open(dot_fp) as file:
         dot_data = file.read()
         dot_graph = Source(dot_data)
-    
+
     # cleanup
     os.remove(dot_fp)
-        
+
     return dot_graph
