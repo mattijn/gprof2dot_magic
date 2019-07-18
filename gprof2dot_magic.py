53113b0bf2ad2078b6274f9a01fbdb7fc24efd76
mattijn <mattijn@gmail.com>
2019-07-18 22:03:58 +0800

    Create gprof2dot_magic.py

---
+++ gprof2dot_magic.py
@@ -0,0 +1,28 @@
+import cProfile
+import gprof2dot as gprof2dot_source
+import sys
+from graphviz import Source
+import tempfile
+from IPython.core.magic import register_line_magic
+
+@register_line_magic
+def gprof2dot(line):
+    # run cProfile and prepare dot file
+    with tempfile.NamedTemporaryFile(suffix='.pstats') as tf:
+        tf_directory = os.path.dirname(tf.name)
+        tf_basename = os.path.basename(tf.name)
+        dot_fp = os.path.join(tf_directory, '{}.dot'.format(tf_name_no_ext))
+        
+        cProfile.run(statement=line, filename=tf.name)
+        sys.argv = ['gprof2dot.py', '-f', 'pstats', '-o', dot_fp, tf.name]
+        gprof2dot_source.main()
+    
+    # read dot file and parse as graph object
+    with open(dot_fp) as file:
+        dot_data = file.read()
+        dot_graph = Source(dot_data)
+    
+    # cleanup
+    os.remove(dot_fp)
+        
+    return dot_graph
